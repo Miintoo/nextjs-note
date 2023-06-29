d449c2a48254e9da8edb49b641c31e8466bc64df
Minto <xpeed96@gmail.com>
2023-06-29 16:14:49 +0900

    Add: metadata

--- src/app/products/layout.tsx
+++ src/app/products/layout.tsx
@@ -1,7 +1,14 @@
+import { Metadata } from 'next';
 import Link from 'next/link';
 
 type Props = {
   children: React.ReactNode;
+}
+
+export const metadata: Metadata = {
+  title: '멋진 제품 사이트 | 전체 제품 확인',
+  description: '멋진 제품을 확인해보세요',
+ 
 }
 
 export default function ProductsLayout({children}: Props) {
