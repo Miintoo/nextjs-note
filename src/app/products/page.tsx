a718305aff11e62291a51a8d86b9f954fc1bda60
Minto <xpeed96@gmail.com>
2023-07-03 07:16:40 +0900

    Add: error.tsx

--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -5,6 +5,7 @@ import Link from 'next/link';
 // export const revalidate = 3;
 
 export default async function ProductsPage() {
+  throw new Error();
   // 서버 파일(DB)에 있는 제품의 리스트를 읽어와서 그걸 보여줌
   const products = await getProducts();
   
