964ae360fa34ef963d80c1ab50a44f200ca1b237
Minto <xpeed96@gmail.com>
2023-07-03 21:49:31 +0900

    Add: middleware 로직

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,7 @@
 import Image from 'next/image'
-import { Inter } from '@next/font/google'
 import styles from './page.module.css'
 import Counter from '@/components/Counter';
 
-const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   console.log('안뇽! = 서버');
@@ -15,7 +13,8 @@ export default function Home() {
       <Image src="https://images.unsplash.com/photo-1441986300917-64674bd600d8"
       alt='shop'
       width={400}
-      height={400} />
+      height={400}
+      priority />
     </>
   )
 }
--- src/app/products/[slug]/page.tsx
+++ src/app/products/[slug]/page.tsx
@@ -1,6 +1,7 @@
+import GoProductsButton from '@/components/GoProductsButton';
 import { getProduct, getProducts } from '@/service/products';
 import Image from 'next/image';
-import { notFound } from 'next/navigation';
+import { notFound, redirect } from 'next/navigation';
 
 export const revalidate = 3;
 
@@ -11,6 +12,8 @@ type Props = {
 }
 
 export function generateMetadata({params}: Props) {
+ 
+
   return {
     title: `제품의 이름 ${params.slug}`
   }
@@ -19,6 +22,10 @@ export function generateMetadata({params}: Props) {
 export default async function ProductPage({params: {slug}}: Props) {
   const product = await getProduct(slug);
 
+  // if (!product) {
+  //   redirect('/products');
+  // }
+
   if (!product) {
     notFound();
   }
@@ -26,6 +33,7 @@ export default async function ProductPage({params: {slug}}: Props) {
   return <>
     <h1>{product.name} 제품 설명 페이지!</h1>;
     <Image src={`/images/${product.image}`} alt={product.name} width={400} height={400}/>
+    <GoProductsButton />
   </>
 }
 
